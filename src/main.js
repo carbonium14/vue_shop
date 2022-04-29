3e1771d5970d1fa5d31a38aa2613f874b2b93060
carbonium14 <1075413520@qq.com>
2022-04-29 20:12:05 +0800

    login finished

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+//全局样式表
+import './assets/css/global.css'
+//字体图标
+import './assets/fonts/iconfont.css'
+//配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/';
+Vue.prototype.$https=axios;
 Vue.config.productionTip = false
 
 new Vue({
