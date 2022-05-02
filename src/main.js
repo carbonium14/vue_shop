657aec1f7886f43056323b1420da4cf67b25bd62
carbonium14 <1075413520@qq.com>
2022-05-02 16:03:21 +0800

    finsih goods cate

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './plugins/element.js'
 import './assets/css/global.css'
 //字体图标
 import './assets/fonts/iconfont.css'
+import ZkTable from 'vue-table-with-tree-grid'
+
 //配置axios
 import axios from 'axios'
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/';
@@ -15,7 +17,7 @@ axios.interceptors.request.use((config)=>{
 })
 Vue.prototype.$https=axios;
 Vue.config.productionTip = false
-
+Vue.component('tree-table', ZkTable)
 new Vue({
   router,
   render: h => h(App)
